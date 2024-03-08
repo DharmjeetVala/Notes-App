73dce435aa39da838f9d47f2fbcd98124fbeeda3
DharmjeetVala <dharmjeetvala@gmail.com>
2024-03-08 15:34:00 +0530

    Search and Splash

---
+++ Notes_Application/app/src/main/java/com/example/notes_application/activities/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.notes_application.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.notes_application.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1705);
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.side_slide);
+        TextView animation = findViewById(R.id.animationtext);
+        animation.startAnimation(anim);
+    }
+}
